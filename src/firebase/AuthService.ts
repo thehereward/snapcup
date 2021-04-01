58d62bfff79fbfe76f22a65b674fe72f2586334f
nrimsky <ninarimsky@gmail.com>
2021-04-01 15:32:35 +0100

    add firebase services

---
+++ src/firebase/AuthService.ts
@@ -0,0 +1,33 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+class AuthService {
+    provider: firebase.auth.OAuthProvider;
+    accessToken: string | undefined;
+    idToken: string | undefined;
+
+    constructor() {
+        this.provider = new firebase.auth.OAuthProvider('microsoft.com');
+        this.provider.setCustomParameters({
+            // Force re-consent.
+            prompt: 'consent',
+            // Only users from a particular Azure AD tenant to sign into the application
+            tenant: 'Softwire.onmicrosoft.com'
+        });
+    }
+
+    async signIn() {
+        try {
+            const result = await firebase.auth().signInWithPopup(this.provider);
+            const credential: firebase.auth.OAuthCredential = result.credential;
+            this.accessToken = credential.accessToken;
+            this.idToken = credential.idToken;
+        } catch(e) {
+            // TODO: Handle error
+            console.log('sign in error')
+            console.error(e);
+        }
+    }
+}
+
+export default AuthService;
