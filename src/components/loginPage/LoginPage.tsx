5a64ad765ecb8c75fc7424c1ab429d8241f090a7
Adam Carruthers <Adam.Carruthers@softwire.com>
2021-04-06 10:05:43 +0100

    Got authentication working!

--- src/components/loginPage/LoginPage.tsx
+++ src/components/loginPage/LoginPage.tsx
@@ -1,17 +1,36 @@
-import React from 'react';
-import AuthService from '../../firebase/AuthService';
+import React, { useCallback, useState, Dispatch, SetStateAction } from "react";
+import AuthService from "../../firebase/AuthService";
 
 interface Props {
-    authService: AuthService
+    authService: AuthService;
+    setLoggedIn: Dispatch<SetStateAction<boolean>>;
 }
 
-const LoginPage:React.FunctionalComponent<Props> = ({authService}) => {
-    return <div className="text-center p-5">
-        <h1>🥤 Welcome to SnapCup!</h1>
-        <p className="m-5 h3">Share those warm fuzzies here!</p>
-        <button onClick={() => authService.signIn()} className="btn btn-primary">Login</button>
-    </div>
-}
+const LoginPage: React.FunctionalComponent<Props> = ({
+    authService,
+    setLoggedIn,
+}) => {
+    const [loginError, setLoginError] = useState("");
+
+    const onSignInSuccess = useCallback(() => {
+        setLoggedIn(true);
+    }, [setLoggedIn]);
+
+    return (
+        <div className="text-center p-5">
+            <h1>🥤 Welcome to SnapCup!</h1>
+            <p className="m-5 h3">Share those warm fuzzies here!</p>
+            <button
+                onClick={() =>
+                    authService.signIn(onSignInSuccess, setLoginError)
+                }
+                className="btn btn-primary"
+            >
+                Login
+            </button>
+            {loginError && <p className="m-2 text-danger">{loginError}</p>}
+        </div>
+    );
+};
 
 export default LoginPage;
-
--- src/firebase/FirestoreService.ts
+++ src/firebase/FirestoreService.ts
@@ -1,5 +1,5 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
+import firebase from "firebase/app";
+import "firebase/firestore";
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
@@ -7,7 +7,7 @@ const firebaseConfig = {
     projectId: process.env.REACT_APP_PROJECT_ID,
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_MEASUREMENT_ID
+    appId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 export function initializeDatabase() {
