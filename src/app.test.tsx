afe18c790bd7ba99d437f723024cabbd961f64a1
Adam Carruthers <adam.j.carruthers@hotmail.co.uk>
2021-04-06 13:39:11 +0100

    Added precommit prettier hook (#12)
    * Added precommit prettier hook
    
    * Removed weird file
    
    * Ran prettier
    
    Co-authored-by: Adam Carruthers <Adam.Carruthers@softwire.com>

--- src/app.test.tsx
+++ src/app.test.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import { render, unmountComponentAtNode } from 'react-dom';
-import { act } from 'react-dom/test-utils';
+import React from "react";
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
 
-import App from './app';
+import App from "./app";
 
 let container = null;
 beforeEach(() => {
-  // setup a DOM element as a render target
-  container = document.createElement("div");
-  document.body.appendChild(container);
+    // setup a DOM element as a render target
+    container = document.createElement("div");
+    document.body.appendChild(container);
 });
 
 afterEach(() => {
-  // cleanup on exiting
-  unmountComponentAtNode(container);
-  container.remove();
-  container = null;
+    // cleanup on exiting
+    unmountComponentAtNode(container);
+    container.remove();
+    container = null;
 });
 
 it("renders without crashing", () => {
     act(() => {
-        render(<App />, container)
+        render(<App />, container);
     });
     expect(container.children.length).toBeGreaterThan(0);
-})
+});
