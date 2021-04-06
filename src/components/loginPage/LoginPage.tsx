ba8ebe8acf51906b5a779bf410a19b2efd53020f
nrimsky <ninarimsky@gmail.com>
2021-04-06 09:14:35 +0100

    fix import

--- src/components/loginPage/LoginPage.tsx
+++ src/components/loginPage/LoginPage.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import AuthService from '~firebase/AuthService';
+import AuthService from '../../firebase/AuthService';
 
 interface Props {
     authService: AuthService
