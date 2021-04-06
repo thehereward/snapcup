09cf8ee1bc81601fa554ca64f4025319a4123d70
nrimsky <ninarimsky@gmail.com>
2021-04-06 17:29:05 +0100

    add login page

--- src/components/loginPage/LoginPage.tsx
+++ src/components/loginPage/LoginPage.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useState, Dispatch, SetStateAction } from "react";
-import {signIn} from '../../firebase/AuthService';
+import { signIn } from "../../firebase/AuthService";
 
 interface Props {
     setLoggedIn: Dispatch<SetStateAction<boolean>>;
@@ -12,7 +12,7 @@ const LoginPage: React.FunctionalComponent<Props> = () => {
         try {
             signIn();
         } catch {
-            setLoginError("There was an error logging in! Please try again.")
+            setLoginError("There was an error logging in! Please try again.");
         }
     }, []);
 
@@ -20,10 +20,7 @@ const LoginPage: React.FunctionalComponent<Props> = () => {
         <div className="text-center p-5">
             <h1>🥤 Welcome to SnapCup!</h1>
             <p className="m-5 h3">Share those warm fuzzies here!</p>
-            <button
-                onClick={() => handleSignIn()}
-                className="btn btn-primary"
-            >
+            <button onClick={() => handleSignIn()} className="btn btn-primary">
                 Login
             </button>
             {loginError && <p className="m-2 text-danger">{loginError}</p>}
