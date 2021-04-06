fe49389564d9033e69c21cb1a5941286e03467ca
nrimsky <ninarimsky@gmail.com>
2021-04-06 17:53:48 +0100

    refactor AuthService

--- src/firebase/AuthService.ts
+++ src/firebase/AuthService.ts
@@ -5,30 +5,48 @@ interface ProfileData {
     isAdmin: boolean;
     isSnapper: boolean;
 }
-// assert user logged in
-// get current user profile
-// if profile not exists create
-// return user profile
-export async function getOrCreateUserProfile(): Promise<ProfileData> {
+
+function assertUserIsLoggedIn() {
     const user = firebase.auth().currentUser;
     if (!user) {
         throw new Error("Should be logged in by here!");
     }
-    const uid = user.uid;
-    const db = firebase.firestore();
-    const docRef = db.collection("users").doc(uid);
+}
+
+async function getCurrentUserProfile(): Promise<ProfileData | null> {
+    const docRef = firebase
+        .firestore()
+        .collection("users")
+        .doc(firebase.auth().currentUser.uid);
     const doc = await docRef.get();
-    let profileData: ProfileData;
     if (doc.exists && doc.data()) {
-        profileData = doc.data() as ProfileData;
+        return doc.data() as ProfileData;
     } else {
-        profileData = {
-            isAdmin: false,
-            isSnapper: true,
-        };
-        db.collection("users").doc(uid).set(profileData);
+        return null;
     }
+}
+
+function createUserProfile(): ProfileData {
+    const profileData = {
+        isAdmin: false,
+        isSnapper: true,
+    };
+    firebase
+        .firestore()
+        .collection("users")
+        .doc(firebase.auth().currentUser.uid)
+        .set(profileData);
     return profileData;
+}
+
+export async function getOrCreateUserProfile(): Promise<ProfileData> {
+    assertUserIsLoggedIn();
+    const currentProfile = getCurrentUserProfile();
+    if (currentProfile) {
+        return currentProfile;
+    } else {
+        return createUserProfile();
+    }
 }
 
 export async function signIn() {
