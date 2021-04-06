529a763e14b66f186503b4c7e998559c3be36400
Adam Carruthers <Adam.Carruthers@softwire.com>
2021-04-06 13:38:15 +0100

    Ran prettier

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,4 @@ import { initializeDatabase } from "./firebase/FirestoreService";
 import App from "./app";
 
 initializeDatabase();
-ReactDOM.render((
-    <App/>
-), document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
