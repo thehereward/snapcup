01bd6eb63b1affd60aba247ce7ea044416d26c2e
nrimsky <ninarimsky@gmail.com>
2021-04-07 15:06:10 +0100

    invert if statement in firebase function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,18 +9,17 @@ export const setFirstUserAsAdmin = functions.firestore
     .onCreate(async (snapshot, context) => {
         const users = await db.collection("users").get();
         const numUsers = users.size;
-        if (numUsers <= 1) {
-            // Grab the current value of what was written to Firestore.
-            const original = snapshot.data().original;
-            functions.logger.log(
-                "Making the first user admin",
-                context.params.documentId,
-                original
-            );
-            const adminUser = { ...original, isAdmin: true };
-            // You must return a Promise when performing asynchronous tasks inside a Functions such as writing to Firestore.
-            return snapshot.ref.set({ adminUser }, { merge: true });
-        } else {
+        if (numUsers > 1) {
             return Promise.resolve();
         }
+        // Grab the current value of what was written to Firestore.
+        const original = snapshot.data().original;
+        functions.logger.log(
+            "Making the first user admin",
+            context.params.documentId,
+            original
+        );
+        const adminUser = { ...original, isAdmin: true };
+        // You must return a Promise when performing asynchronous tasks inside a Functions such as writing to Firestore.
+        return snapshot.ref.set({ adminUser }, { merge: true });
     });
