7c30896de19e72eca715f60bd38b97e7600eaaf9
beattyemily <81611322+beattyemily@users.noreply.github.com>
2021-04-07 10:22:12 +0100

    Submission text box with database connection (#15)
    * setting up jest for use with ts
    
    * instructions to readme for automatic re-runs
    
    * Update tsconfig.json
    
    Co-authored-by: Hereward <thehereward@users.noreply.github.com>
    
    * pipeline for tests on pull/merge
    
    * spelling error correction
    
    * syntax corrections
    
    * syntax correction
    
    * syntax correction
    
    * comment adding
    
    * change comment
    
    * add tests to merge pipeline
    
    * remove tests from merge pipeline
    
    * split tests
    
    * initial commit for text box component
    
    * first bits of firebase implementation
    
    * tidying up README and index
    
    * changing back to viewing app from submission text box
    
    * configuring database
    
    * deleting redundant message type
    
    * removed state from charactersLeft
    
    * adding submission text box to index
    
    * updating packages
    
    * edits for merging with upstream
    
    * incorporating submission text box after login
    
    * Delete .env
    
    * minor edits for merging
    
    * Update src/components/submissionPage/SubmissionTextBox.tsx
    
    Co-authored-by: Hereward <thehereward@users.noreply.github.com>
    
    * Update src/components/submissionPage/SubmissionTextBox.tsx
    
    Co-authored-by: Hereward <thehereward@users.noreply.github.com>
    
    * Update src/firebase/snap/SubmitSnap.ts
    
    Co-authored-by: Hereward <thehereward@users.noreply.github.com>
    
    * Update src/firebase/snap/SubmitSnap.ts
    
    Co-authored-by: Hereward <thehereward@users.noreply.github.com>
    
    * bug fix in STB
    
    * Update src/components/submissionPage/SubmissionTextBox.tsx
    
    Co-authored-by: Hereward <thehereward@users.noreply.github.com>
    
    * remove esModuleInterop
    
    * change snap recipient initial format
    
    Co-authored-by: Emily Beatty <emily.beatty@softwire.com>
    Co-authored-by: Hereward <thehereward@users.noreply.github.com>

---
+++ src/firebase/snap/Snap.ts
@@ -0,0 +1,7 @@
+/* Snap Structure */
+export default interface Snap {
+    to: string[];  /* Recipient UIDs */
+    from: string;   /* Sender UID */
+    body: string;   /* Main Message */
+    timestamp: Date;   /* Date/time of Submission*/
+}
