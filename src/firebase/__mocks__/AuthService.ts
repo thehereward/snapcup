d103f8aed1d6a0674510bce0d40df94ed65b5578
nrimsky <ninarimsky@gmail.com>
2021-04-07 09:43:01 +0100

    mock firebase

---
+++ src/firebase/__mocks__/AuthService.ts
@@ -0,0 +1,24 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+export interface ProfileData {
+    isAdmin: boolean;
+    isSnapper: boolean;
+}
+
+export async function signIn() {
+    return;
+}
+
+export async function signOut() {
+    return;
+}
+
+export function getCurrentUserName(): String {
+    return "Henry";
+}
+
+export function getUserProfile(cb: (p: ProfileData) => void) {
+    const mockProfile = {isAdmin: true, isSnapper: true};
+    cb(mockProfile);
+}
