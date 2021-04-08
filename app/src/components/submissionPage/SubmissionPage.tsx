e9d36b50d825cde0fd4c08788d399b2fb800119b
nrimsky <ninarimsky@gmail.com>
2021-04-08 14:04:17 +0100

    amend import statement

--- app/src/components/submissionPage/SubmissionPage.tsx
+++ app/src/components/submissionPage/SubmissionPage.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import SubmissionTextBox from "./SubmissionTextBox";
-import { getCurrentUserName } from "../../firebase/AuthService";
+import { getCurrentUserName } from "../../firebase/users/UserService";
 import YourSnaps from "./YourSnaps";
 
 const SubmissionPage = () => {
