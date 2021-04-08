72c10480d72bb7983640a52dcd2e1aaaa65aaa1a
Emily Beatty <emily.beatty@softwire.com>
2021-04-08 16:37:19 +0100

    frontend framework for cup creation functionality

---
+++ app/src/components/adminConsole/currentCup/CurrentCupHeader.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import CreateCupButton from "./CreateCupButton";
+const CurrentCupHeader: React.FunctionComponent = (props: {
+    isCup: Boolean;
+    isOpen: Boolean;
+}) => {
+    return (
+        <div>
+            <h5>Current SnapCups</h5>
+            <CreateCupButton isCup={props.isCup} isOpen={props.isOpen} />
+        </div>
+    );
+};
+
+export default CurrentCupHeader;
