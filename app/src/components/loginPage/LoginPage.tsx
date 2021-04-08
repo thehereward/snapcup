7b2b3120baca7e870873813cbc96c23ff4a7f4a1
Adam Carruthers <Adam.Carruthers@softwire.com>
2021-04-08 14:55:02 +0100

    Added spicy login page

--- app/src/components/loginPage/LoginPage.tsx
+++ app/src/components/loginPage/LoginPage.tsx
@@ -1,30 +1,100 @@
 import React, { useCallback, useState, Dispatch, SetStateAction } from "react";
 import { signIn } from "../../firebase/AuthService";
+import styled from "styled-components";
+// @ts-ignore
+import Elle from "./Elle.svg";
 
 interface Props {
     setLoggedIn: Dispatch<SetStateAction<boolean>>;
 }
 
+const LoginBox = styled.div`
+    min-height: 100vh;
+    padding: 50px 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    width: 100%;
+    max-width: 800px;
+    margin: 0 auto;
+
+    @media only screen and (max-width: 576px) {
+        max-width: 350px;
+    }
+`;
+
+const FormColumn = styled.div`
+    color: var(--purp-selected);
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding-top: 20px;
+
+    h1 {
+        font-family: var(--open-sans);
+        font-weight: 800;
+        font-size: 50px;
+    }
+
+    p {
+        font-family: var(--asap);
+        font-weight: normal;
+        font-size: 22px;
+    }
+`;
+
+const ElleImg = styled.img`
+    width: 100%;
+    max-height: calc(100vh - 50px);
+
+    @media only screen and (max-width: 576px) {
+        max-width: 250px;
+    }
+`;
+
 const LoginPage: React.FunctionalComponent<Props> = () => {
     const [loginError, setLoginError] = useState("");
 
     const handleSignIn = useCallback(() => {
-        try {
-            signIn();
-        } catch {
-            setLoginError("There was an error logging in! Please try again.");
-        }
+        (async () => {
+            try {
+                await signIn();
+            } catch {
+                setLoginError(
+                    "There was an error logging in! Please try again."
+                );
+            }
+        })();
     }, []);
 
     return (
-        <div className="text-center p-5">
-            <h1>🥤 Welcome to SnapCup!</h1>
-            <p className="m-5 h3">Share those warm fuzzies here!</p>
-            <button onClick={() => handleSignIn()} className="btn btn-primary">
-                Login
-            </button>
-            {loginError && <p className="m-2 text-danger">{loginError}</p>}
-        </div>
+        <LoginBox>
+            <div className="flex-grow-1" />
+            <div className="container-fluid">
+                <div className="row">
+                    <FormColumn className="col-12 col-sm-6 order-2 order-sm-1">
+                        <h1>SnapCup</h1>
+                        <p>Send compliments to colleagues!</p>
+
+                        <button
+                            role="button"
+                            className="btn btn-elle btn-lg mt-4"
+                            onClick={handleSignIn}
+                        >
+                            Log In
+                        </button>
+                        {loginError && <i className="mt-2">{loginError}</i>}
+                    </FormColumn>
+
+                    <div className="col-12 col-sm-6 text-center order-1 order-sm-2">
+                        <ElleImg src={Elle} className="w-100" />
+                    </div>
+                </div>
+            </div>
+
+            <div className="flex-grow-1" />
+        </LoginBox>
     );
 };
 
