bbe750eb8006dfe7debd909deda58d107dfad695
nrimsky <ninarimsky@gmail.com>
2021-04-08 11:02:26 +0100

    use cards

--- app/src/components/submissionPage/SnapList.tsx
+++ app/src/components/submissionPage/SnapList.tsx
@@ -6,11 +6,23 @@ interface SnapListProps {
 }
 
 const SnapList: React.FunctionComponent<SnapListProps> = ({ snaps }) => {
+    function formatAts(ats: string[]): string {
+        return ats.map((s) => `@${s}`).join(" ");
+    }
+
     const listItems = snaps.map((snap: Snap, index: number) => (
-        <li key={index}>{snap.body}</li>
+        <div className="col-sm-6 mb-2" key={index}>
+            <div className="card">
+                <div className="card-body">
+                    <h5 className="card-title">{formatAts(snap.to)}</h5>
+                    <p className="card-text">{snap.body}</p>
+                </div>
+                <div className="card-footer text-muted">{"8 April 2021"}</div>
+            </div>
+        </div>
     ));
 
-    return <ul>{listItems}</ul>;
+    return <div className="row">{listItems}</div>;
 };
 
 export default SnapList;
