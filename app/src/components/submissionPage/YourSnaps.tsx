0dddff4aee24afeacefe778bf8829d6df7f016bb
nrimsky <ninarimsky@gmail.com>
2021-04-08 17:18:16 +0100

    elle woods header

--- app/src/components/submissionPage/YourSnaps.tsx
+++ app/src/components/submissionPage/YourSnaps.tsx
@@ -3,6 +3,25 @@ import { useState, useEffect } from "react";
 import Snap from "../../types/Snap";
 import { streamSubmittedSnapsForCurrentUser } from "../../firebase/snaps/SnapService";
 import SnapList from "./SnapList";
+import styled, { css } from "styled-components";
+// @ts-ignore
+import Elle from "../loginPage/Elle.svg";
+
+const YourSnapsHeader = styled.h2`
+    font-family: Asap;
+    font-weight: 500;
+    font-size: 30px;
+    line-height: 34px;
+    color: var(--elle);
+    margin-bottom: 25px;
+`;
+
+const MiniElleImg = styled.img`
+    height: 66px;
+    width: auto;
+    vertical-align: text-bottom;
+    margin-right: 19px;
+`;
 
 const YourSnaps: React.FunctionComponent = () => {
     const [snaps, setSnaps] = useState<Snap[]>([]);
@@ -19,7 +38,10 @@ const YourSnaps: React.FunctionComponent = () => {
 
     return (
         <div>
-            <h2>Your snaps:</h2>
+            <YourSnapsHeader>
+                <MiniElleImg src={Elle} />
+                SnapCups
+            </YourSnapsHeader>
             {error && <p>Error: {error}</p>}
             {snaps.length > 0 && <SnapList snaps={snaps} />}
         </div>
