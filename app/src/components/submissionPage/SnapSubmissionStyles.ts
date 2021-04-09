9fa9ee5fa4a6f27f51f664ff35dec74da9507f11
Elijah Ahmad <aa1719@ic.ac.uk>
2021-04-09 15:00:36 +0100

    Kept variable names, but updated values for purple-button-hover.

--- app/src/components/submissionPage/SnapSubmissionStyles.ts
+++ app/src/components/submissionPage/SnapSubmissionStyles.ts
@@ -91,12 +91,12 @@ export const SnapItButton = styled.button`
         box-shadow: 2px 4px 4px rgba(0, 0, 0, 0.25);
     }
     &:focus {
-        background-color: var(--purple-selected);
+        background-color: var(--purple-button-focus);
         border: 0px;
         box-shadow: none;
     }
     &:active {
-        background-color: var(--purple-selected);
+        background-color: var(--purple-button-focus);
         border: 0px;
         box-shadow: none;
     }
