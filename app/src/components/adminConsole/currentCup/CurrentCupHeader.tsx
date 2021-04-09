3aa0e60e86d98eadf80127b0696f31e1fda3ad8a
Emily Beatty <emily.beatty@softwire.com>
2021-04-09 13:42:54 +0100

    styling header

--- app/src/components/adminConsole/currentCup/CurrentCupHeader.tsx
+++ app/src/components/adminConsole/currentCup/CurrentCupHeader.tsx
@@ -1,12 +1,34 @@
 import React from "react";
 import CreateCupButton from "./CreateCupButton";
+import styled, { css } from "styled-components";
+import Elle from "../../../images/Elle.svg";
+
+const YourSnapsHeader = styled.h2`
+    font-family: Asap;
+    font-weight: 500;
+    font-size: 30px;
+    line-height: 34px;
+    color: var(--purple-selected);
+    margin-bottom: 25px;
+`;
+
+const MiniElleImg = styled.img`
+    height: 66px;
+    width: auto;
+    vertical-align: text-bottom;
+    margin-right: 19px;
+`;
+
 const CurrentCupHeader: React.FunctionComponent = (props: {
     isCup: Boolean;
     isOpen: Boolean;
 }) => {
     return (
         <div>
-            <h5>Current SnapCups</h5>
+            <YourSnapsHeader>
+                <MiniElleImg src={Elle} />
+                Current SnapCup
+            </YourSnapsHeader>
             <CreateCupButton isCup={props.isCup} isOpen={props.isOpen} />
         </div>
     );
