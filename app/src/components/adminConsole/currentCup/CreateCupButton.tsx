cafbad96cc6e712699492e6a0ace54fa378f9d8e
Emily Beatty <emily.beatty@softwire.com>
2021-04-09 11:09:48 +0100

    submitting new cups to firebase

--- app/src/components/adminConsole/currentCup/CreateCupButton.tsx
+++ app/src/components/adminConsole/currentCup/CreateCupButton.tsx
@@ -1,4 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { GetCupNames } from "../../../firebase/cups/CupService";
+import { createNewCup } from "../../../firebase/cups/CupService";
+import Cup from "../../../types/Cup";
 
 const LOADING = "loading";
 const IDLE = "idle";
@@ -9,17 +12,53 @@ const CreateCupButton: React.FunctionComponent = (props: {
     isOpen: Boolean;
 }) => {
     const [status, setStatus] = useState({ status: IDLE });
-    const createCup = () => {
-        console.log("cup created");
+    const [newCupName, setNewCupName] = useState<String>("");
+    const [allCupNames, setAllCupNames] = useState<String[]>([]);
+
+    useEffect(() => {
+        GetCupNames()
+            .then((res: String[]) => {
+                setAllCupNames(res);
+            })
+            .catch((e) => console.log(e));
+    }, [GetCupNames, setAllCupNames]);
+
+    const handleCreateClick = () => {
+        setStatus({ status: LOADING });
+        const newCup: Cup = {
+            isPublished: false,
+            isOpen: false,
+            timeCreated: new Date(),
+            name: newCupName,
+        };
+        try {
+            createNewCup(newCup);
+            setNewCupName("");
+            //TODO: change isCup
+        } catch (error) {
+            console.log(error.toString());
+            console.log("error in firebase");
+        }
+        setStatus({ status: IDLE });
     };
+
+    const handleNCNChange = (event) => {
+        setNewCupName(event.target.value);
+    };
+
     if (!props.isCup) {
         return (
             <div>
+                <input value={newCupName} onChange={handleNCNChange} />
                 <button
                     type="button"
                     className="btn-createCup"
-                    onClick={() => createCup()}
-                    disabled={status.status === LOADING}
+                    onClick={() => handleCreateClick()}
+                    disabled={
+                        status.status === LOADING ||
+                        newCupName.length == 0 ||
+                        allCupNames.includes(newCupName)
+                    }
                 >
                     + New SnapCup
                 </button>
