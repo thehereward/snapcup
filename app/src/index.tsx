d7f48f67ab918e0f1eb641c42b82828d07d097ab
Emily Beatty <emily.beatty@softwire.com>
2021-04-09 17:35:44 +0100

    edits for rebasing

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { initializeDatabase } from "./firebase/InitializeDatabase";
 import AppWrap from "./appWrap";
-import Popup from "react-popup";
 
 initializeDatabase();
 ReactDOM.render(<AppWrap />, document.getElementById("root"));
