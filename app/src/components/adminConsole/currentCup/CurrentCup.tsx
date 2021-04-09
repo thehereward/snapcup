1596a6b1217e3579f3c044aa4058aa8659d2202d
Emily Beatty <emily.beatty@softwire.com>
2021-04-09 17:21:19 +0100

    Creating Options Bar

--- app/src/components/adminConsole/currentCup/CurrentCup.tsx
+++ app/src/components/adminConsole/currentCup/CurrentCup.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import AdminCurrentSnapsDisplay from "./AdminCurrentSnapsDisplay";
 import CurrentCupHeader from "./CurrentCupHeader";
+import { SectionHeaderUnderline } from "../AdminConsoleStyles";
 
 const CurrentCup: React.FunctionComponent = (props: {
     isCup: Boolean;
@@ -13,6 +14,7 @@ const CurrentCup: React.FunctionComponent = (props: {
                 isCup={props.isCup}
                 updateIsCup={props.updateIsCup}
             />
+            <SectionHeaderUnderline />
             <AdminCurrentSnapsDisplay
                 isCup={props.isCup}
                 isOpen={props.isOpen}
