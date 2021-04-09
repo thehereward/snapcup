18d2aa0e5a3447424bfa7091dc1a3549b20d9cdb
Emily Beatty <emily.beatty@softwire.com>
2021-04-09 11:16:31 +0100

    fixing merge conflicts

--- app/src/components/adminConsole/currentCup/CurrentCupHeader.tsx
+++ app/src/components/adminConsole/currentCup/CurrentCupHeader.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import CreateCupButton from "./CreateCupButton";
-
 const CurrentCupHeader: React.FunctionComponent = (props: {
     isCup: Boolean;
     isOpen: Boolean;
