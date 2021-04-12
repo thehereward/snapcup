2b8667a98d1dbd190bf5752fd7dc8f0aab1a8d88
Nina Rimsky <57836187+nrimsky@users.noreply.github.com>
2021-04-12 17:55:54 +0100

    📋 Add snappable table for admins (#69)
    * add snappables table
    
    * add snaps received placeholder
    
    * fix position of download button

--- app/src/firebase/users/GetSnappables.ts
+++ app/src/firebase/users/GetSnappables.ts
@@ -1,15 +1,35 @@
 import firebase from "firebase/app";
 import Snappable from "../../types/Snappable";
 
+function docToSnappable(
+    docSnapshot: firebase.firestore.QueryDocumentSnapshot<firebase.firestore.DocumentData>
+): Snappable {
+    const { email, fullName, username } = docSnapshot.data();
+    return { id: docSnapshot.id, email, fullName, username };
+}
+
 /* Gets a list of Nicknames that are unique */
 export default async function getSnappables(): Promise<Snappable[]> {
     const querySnapshot = await firebase
         .firestore()
         .collection("snappables")
         .get({ source: "server" });
-    const result = querySnapshot.docs.map((doc) => {
-        const { email, fullName, username } = doc.data();
-        return { id: doc.id, email, fullName, username };
-    });
+    const result = querySnapshot.docs.map(docToSnappable);
     return result;
+}
+
+export function streamAllSnappablePeople(
+    onPeopleReceived: (people: Snappable[]) => void,
+    onError: (error: Error) => void
+): () => void {
+    const collectionRef = firebase.firestore().collection("snappables");
+    return collectionRef.onSnapshot({
+        next: (querySnapshot) => {
+            const updated = querySnapshot.docs.map(docToSnappable);
+            onPeopleReceived(updated);
+        },
+        error: (error) => {
+            onError(error);
+        },
+    });
 }
