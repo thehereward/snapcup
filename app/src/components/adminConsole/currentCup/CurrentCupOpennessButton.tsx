06ded484f2b9e4cc11b2fbdc42074aa514d2bf26
Adam Carruthers <Adam.Carruthers@softwire.com>
2021-04-13 16:28:35 +0100

    added animation to button

--- app/src/components/adminConsole/currentCup/CurrentCupOpennessButton.tsx
+++ app/src/components/adminConsole/currentCup/CurrentCupOpennessButton.tsx
@@ -5,26 +5,36 @@ import { setCupOpenness } from "../../../firebase/cups/CupService";
 const CurrentCupOpennessButton = ({ cup }) => {
     const [loading, setLoading] = useState<boolean>(false);
     const [error, setError] = useState<string>("");
+    const [success, setSuccess] = useState<boolean>(false);
 
     let buttonText = `${cup.isOpen ? "Close" : "Open"} SnapCup`;
     if (error) {
         buttonText += "- " + error;
     }
+    if (loading) {
+        buttonText = "Loading";
+    }
 
-    const clickSetOpenness = useCallback(async () => {
+    const clickSetOpenness = async () => {
         try {
             setLoading(true);
             await setCupOpenness(cup.id, !cup.isOpen);
             setLoading(false);
             setError("");
+            setSuccess(true);
+            setTimeout(() => setSuccess(false), 500);
         } catch (err) {
             console.error(err);
             setError("Error!");
         }
-    }, [setLoading, setError]);
+    };
 
     return (
-        <CurrentCupOptionsButton disabled={loading} onClick={clickSetOpenness}>
+        <CurrentCupOptionsButton
+            className={success && "success"}
+            disabled={loading}
+            onClick={clickSetOpenness}
+        >
             {buttonText}
         </CurrentCupOptionsButton>
     );
