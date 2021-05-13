abbc78d8ba6b30e161880d0b939217dfd194ed7a
Hereward Mills <hereward.mills@softwire.com>
2021-05-13 23:54:36 +0100

    Move global styles to scss.

--- app/src/appWrap.tsx
+++ app/src/appWrap.tsx
@@ -1,21 +1,9 @@
-/*
- * A component for global, unchanging tags needed later.
- */
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import { createGlobalStyle } from "styled-components";
 import App from "./app";
-
-const GlobalStyle = createGlobalStyle`
-body {
-    background: linear-gradient(240.3deg, rgba(58, 9, 161, 0.4) -17.93%, rgba(196, 196, 196, 0) 100.75%);
-    min-height: 100vh;
-}
-`;
 
 const AppWrap = () => (
     <Router>
-        <GlobalStyle />
         <App />
     </Router>
 );
