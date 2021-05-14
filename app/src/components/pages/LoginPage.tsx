049e2e23be5ab12e907346450444942d0a2f86e9
Hereward Mills <hereward.mills@softwire.com>
2021-05-14 22:52:25 +0100

    Improve login page styling.

--- app/src/components/pages/LoginPage.tsx
+++ app/src/components/pages/LoginPage.tsx
@@ -1,6 +1,5 @@
 import React, { useCallback, useState, Dispatch, SetStateAction } from "react";
 import { signIn } from "../../firebase/users/UserService";
-import styled from "styled-components";
 // @ts-ignore
 import Elle from "../../images/Elle";
 import Loading from "../Loading";
@@ -8,51 +7,6 @@ import Loading from "../Loading";
 interface Props {
     setLoggedIn: Dispatch<SetStateAction<boolean>>;
 }
-
-const LoginBox = styled.div`
-    min-height: 100vh;
-    padding: 50px 0;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    width: 100%;
-    max-width: 800px;
-    margin: 0 auto;
-
-    @media only screen and (max-width: 576px) {
-        max-width: 350px;
-    }
-`;
-
-const FormColumn = styled.div`
-    color: var(--purple-selected);
-
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    padding-top: 20px;
-
-    h1 {
-        font-family: var(--open-sans);
-        font-weight: 800;
-        font-size: 50px;
-    }
-
-    p {
-        font-family: var(--asap);
-        font-weight: normal;
-        font-size: 22px;
-    }
-`;
-
-const ElleImg = styled(Elle)`
-    width: 100%;
-    max-height: calc(100vh - 50px);
-
-    @media only screen and (max-width: 576px) {
-        max-width: 250px;
-    }
-`;
 
 const LoginPage: React.FunctionalComponent<Props> = () => {
     const [loginError, setLoginError] = useState("");
@@ -72,14 +26,12 @@ const LoginPage: React.FunctionalComponent<Props> = () => {
     return loading ? (
         <Loading />
     ) : (
-        <LoginBox>
-            <div className="flex-grow-1" />
+        <div className="login-box">
             <div className="container-fluid">
                 <div className="row">
-                    <FormColumn className="col-12 col-sm-6 order-2 order-sm-1">
+                    <div className="col-12 col-sm-6 order-2 order-sm-1 d-flex flex-column justify-content-center pt-4 login-page-column">
                         <h1>Snap Cup</h1>
                         <p>Send compliments to colleagues!</p>
-
                         <button
                             role="button"
                             className="btn btn-purple btn-lg mt-4"
@@ -88,16 +40,14 @@ const LoginPage: React.FunctionalComponent<Props> = () => {
                             Log In
                         </button>
                         {loginError && <i className="mt-2">{loginError}</i>}
-                    </FormColumn>
+                    </div>
 
                     <div className="col-12 col-sm-6 text-center order-1 order-sm-2">
-                        <ElleImg />
+                        <Elle className="login-page-elle-image" />
                     </div>
                 </div>
             </div>
-
-            <div className="flex-grow-1" />
-        </LoginBox>
+        </div>
     );
 };
 
