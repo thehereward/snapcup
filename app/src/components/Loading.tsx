b19e722a3173e05c0a2382bc157e3610c2d6dc5b
Hereward Mills <hereward.mills@softwire.com>
2021-05-14 22:33:04 +0100

    Improve loading page styling.

--- app/src/components/Loading.tsx
+++ app/src/components/Loading.tsx
@@ -1,32 +1,17 @@
 import React from "react";
-import styled from "styled-components";
 // @ts-ignore
 import Elle from "../images/Elle";
 
-const LoadingBox = styled.div`
-    position: relative;
-    width: 100%;
-    max-width: 300px;
-    margin: auto;
-    height: 100vh;
-`;
-
-const ElleImg = styled(Elle)`
-    width: 100%;
-    height: auto;
-    padding: 40px;
-`;
-
 const Loading = () => {
     return (
-        <LoadingBox>
-            <ElleImg />
+        <div className="single-column">
+            <Elle />
             <div className="d-flex justify-content-center">
                 <div className="spinner-border mt-3 text-purple" role="status">
                     <span className="sr-only">Loading...</span>
                 </div>
             </div>
-        </LoadingBox>
+        </div>
     );
 };
 
