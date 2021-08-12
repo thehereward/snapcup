eff46a7049e2cc4ed82761198647c8d0101e6ac1
Hereward Mills <hereward.mills@softwire.com>
2021-08-12 09:08:37 +0100

    Rename misnamed file.

--- app/src/components/submissionPage/SubmissionBoxErrorDisplay.test.tsx
+++ app/src/components/submissionPage/SubmissionBoxErrorDisplay.test.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { render } from "@testing-library/react";
-import { screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import SubmissionBoxErrorDisplay from "./SubmissionBoxErrorDisplay";
 
 describe("SubmissionBoxErrorDisplay", () => {
