e67f9968172fcccaeff4fd7cadd2f422a8b00101
Hereward Mills <herewardnjmills@gmail.com>
2022-02-26 20:05:15 +0000

    feature: add links to view snaps and manage published cups

--- app/src/components/adminConsole/publishedCups/SingleCupCard.tsx
+++ app/src/components/adminConsole/publishedCups/SingleCupCard.tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { Cup, Entity } from "../../../types";
 import ExportSnaps from "../ExportSnaps";
+import { useDownloadSnaps, STATUS } from "../csvTools";
 
 function formatTimePublished(date: Date): string {
     return (
@@ -15,28 +16,39 @@ function formatTimePublished(date: Date): string {
 }
 
 const SingleCupCard = (props: { cup: Entity<Cup> }) => {
+    const { status, downloadSnaps } = useDownloadSnaps(props.cup.id);
     const formattedPublishedTime =
         props.cup.timePublished == undefined
             ? ""
             : formatTimePublished(props.cup.timePublished);
 
-    const navigate = useNavigate();
-    function onClick() {
-        navigate(`/cups/${props.cup.id}`);
-    }
-
     return (
         <div className="col-sm-6 col-lg-4 mb-4" key={props.cup.id}>
-            <div
-                className="bg-colour-highlight-1 cursor_pointer bg-colour-secondary-on-hover h-100 rounded-3 shadow-lg"
-                onClick={onClick}
-            >
+            <div className="bg-colour-highlight-1 h-100 rounded-3 shadow-lg">
                 <div className="fw-bold font-family-open-sans colour-text-normal d-flex flex-column w-100 h-100 p-4">
                     <p className="font-size-18 mb-1">{props.cup.name}</p>
                     <p className="font-size-14 mb-1">
                         {formattedPublishedTime}
                     </p>
-                    <ExportSnaps cup={props.cup} />
+                    <Link
+                        to={`/manage/cups/${props.cup.id}`}
+                        className="btn button-secondary rounded-pill font-size-16 text-decoration-none my-1"
+                    >
+                        Manage Cup
+                    </Link>
+                    <Link
+                        to={`/cups/${props.cup.id}`}
+                        className="btn button-secondary rounded-pill font-size-16 text-decoration-none my-1"
+                    >
+                        View Snaps
+                    </Link>
+                    <button
+                        className={`button-secondary rounded-pill font-size-16 my-1`}
+                        onClick={downloadSnaps}
+                        disabled={status === STATUS.LOADING}
+                    >
+                        Download Snaps
+                    </button>
                 </div>
             </div>
         </div>
